ec3261e96551cfe0680cfd9a9c2ba939472ae097
Dobromir Stefanov Ivanov <lalilulelo.mg@gmail.com>
2024-02-02 19:23:58 +1100

    Add templ generation and server setup

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/Captain-Leftovers/beekeepers_log_go_htmx/view/layout"
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -11,7 +18,38 @@ func main() {
 
 	godotenv.Load(".env")
 
-	jwtSecret := os.Getenv("JWT_SECRET")
+	_ = os.Getenv("JWT_SECRET")
 
-	fmt.Println(jwtSecret)
+	port := os.Getenv("PORT")
+
+	portInt, err := strconv.Atoi(port)
+
+	if err != nil {
+		log.Fatalf("Port must be a number of a PORT but Got -> %v", port)
+	}
+
+	mainRouter := chi.NewRouter()
+
+	mainRouter.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	mainRouter.Handle("/", templ.Handler(layout.Base("Hello World!")))
+
+	fmt.Println("Server is running on http://localhost:" + port)
+
+	err = http.ListenAndServe(fmt.Sprintf(":%d", portInt), mainRouter)
+
+	if err != nil {
+		fmt.Println("Error while starting the server")
+		fmt.Println(err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/Captain-Leftovers/beekeepers_log_go_htmx
 
 go 1.21.6
 
-require github.com/joho/godotenv v1.5.1
-
-require github.com/a-h/templ v0.2.543 // indirect
+require (
+	github.com/a-h/templ v0.2.543
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
